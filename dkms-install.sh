b4ed17a1d4fd9241857fce0d5a4d86b628d932d0
Deokgyu Yang <secugyu@gmail.com>
2022-01-26 16:37:57 +0900

    Add DKMS configuration and the helper scripts to install/uninstall
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>

---
+++ dkms-install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "You must run this with superuser priviliges.  Try \"sudo ./dkms-install.sh\"" 2>&1
+  exit 1
+else
+  echo "About to run dkms install steps..."
+fi
+
+DRV_DIR="$(pwd)"
+DRV_NAME=r8152
+DRV_VERSION=2.15.0
+
+cp -r ${DRV_DIR} /usr/src/${DRV_NAME}-${DRV_VERSION}
+
+dkms add -m ${DRV_NAME} -v ${DRV_VERSION}
+dkms build -m ${DRV_NAME} -v ${DRV_VERSION}
+dkms install -m ${DRV_NAME} -v ${DRV_VERSION}
+RESULT=$?
+
+echo "Finished running dkms install steps."
+
+echo "Copy the dedicated udev rules file..."
+cp udev/rules.d/50-usb-realtek-net.rules /etc/udev/rules.d/
+
+echo "Restarting udev..."
+udevadm control --reload-rules
+
+echo "Finished."
+
+exit $RESULT
---
+++ dkms-remove.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "You must run this with superuser priviliges.  Try \"sudo ./dkms-remove.sh\"" 2>&1
+  exit 1
+else
+  echo "About to run dkms removal steps..."
+fi
+
+DRV_DIR="$(pwd)"
+DRV_NAME=r8152
+DRV_VERSION=2.15.0
+
+dkms remove ${DRV_NAME}/${DRV_VERSION} --all
+rm -rf /usr/src/${DRV_NAME}-${DRV_VERSION}
+
+RESULT=$?
+if [[ "$RESULT" != "0" ]]; then
+  echo "Error occurred while running dkms remove." 2>&1
+else
+  echo "Finished running dkms removal steps."
+fi
+
+echo "Removing the dedicated udev rules file..."
+rm /etc/udev/rules.d/50-usb-realtek-net.rules
+
+echo "Restarting udev..."
+udevadm control --reload-rules
+
+echo "Finished."
+
+exit $RESULT
